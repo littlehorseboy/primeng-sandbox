b24798693d921be7b872d849307b5ebdad7e9855
馬文彬 <a35434@mail.cmuh.org.tw>
2021-11-05 09:09:21 +0800

    feat(): p-tree practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TreeDemoModule } from './tree-demo/tree-demo.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TreeDemoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
